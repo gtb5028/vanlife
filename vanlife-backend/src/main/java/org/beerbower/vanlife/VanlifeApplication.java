33d0d4004f50a39c2eeca35b53b867dccf3a6772
Greg Beerbower <gtb5028@gmail.com>
2025-02-17 12:56:26 -0800

    Initial backend commit

---
+++ vanlife-backend/src/main/java/org/beerbower/vanlife/VanlifeApplication.java
@@ -0,0 +1,13 @@
+package org.beerbower.vanlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanlifeApplication.class, args);
+	}
+
+}
