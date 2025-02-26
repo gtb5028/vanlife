a09abf86840f5b220693493d84d5d02f02bde898
Tom Beerbower <tbeerbower@gmail.com>
2025-02-26 15:17:33 -0500

    Feature/rbac (#28)
    * user controller authenticated access
    
    * location controller access control

--- vanlife-backend/src/main/java/org/beerbower/vanlife/controllers/LocationController.java
+++ vanlife-backend/src/main/java/org/beerbower/vanlife/controllers/LocationController.java
@@ -2,12 +2,15 @@ package org.beerbower.vanlife.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.beerbower.vanlife.entities.Location;
+import org.beerbower.vanlife.entities.User;
 import org.beerbower.vanlife.repositories.LocationRepository;
+import org.beerbower.vanlife.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -17,30 +20,41 @@ import java.util.List;
 @RequiredArgsConstructor
 public class LocationController {
     private final LocationRepository locationRepository;
+    private final UserRepository userRepository;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public List<Location> getAllLocations() {
         return locationRepository.findAll();
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
     public Location getSingleLocation(@PathVariable long id) {
         return locationRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Location createLocation(@RequestBody Location location) {
+    public Location createLocation(@RequestBody Location location, Principal principal) {
+        User user = userRepository.findByEmail(principal.getName()).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+
+        location.setId(null);
+        location.setCreatedBy(user);
+        location.setCreatedAt(null);
+        location.setUpdatedAt(null);
         return locationRepository.save(location);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteLocation(@PathVariable long id) {
         if (!locationRepository.existsById(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-
         locationRepository.deleteById(id);
     }
 }
--- vanlife-backend/src/main/resources/data.sql
+++ vanlife-backend/src/main/resources/data.sql
@@ -6,7 +6,7 @@ INSERT INTO usr (id, email, name, picture, active, roles, password) VALUES
 (4, 'alice.jones@example.com', 'Alice Jones', '', true, 'ROLE_USER', '$2a$10$TXkorQjx0GhjdjgJ2A84.OQ5W3Q5OWWu.SXXCKjyDt.vXD2WdzxyS'),
 (5, 'john.doe@example.com', 'John Doe', '', true, 'ROLE_USER', '$2a$10$TXkorQjx0GhjdjgJ2A84.OQ5W3Q5OWWu.SXXCKjyDt.vXD2WdzxyS'),
 (6, 'kate@example.com', 'Kate Spate', '', true, 'ROLE_USER', '$2a$10$TXkorQjx0GhjdjgJ2A84.OQ5W3Q5OWWu.SXXCKjyDt.vXD2WdzxyS'),
-(7, 'gtb5028@gmail.com', 'Greg Beerbower', '', true, 'ROLE_USER', '$2a$10$TXkorQjx0GhjdjgJ2A84.OQ5W3Q5OWWu.SXXCKjyDt.vXD2WdzxyS');
+(7, 'gtb5028@gmail.com', 'Greg Beerbower', '', true, 'ROLE_ADMIN', '$2a$10$TXkorQjx0GhjdjgJ2A84.OQ5W3Q5OWWu.SXXCKjyDt.vXD2WdzxyS');
 
 -- Reset the sequence value to avoid primary key conflicts
 SELECT setval('usr_id_seq', (SELECT MAX(id) FROM usr));
