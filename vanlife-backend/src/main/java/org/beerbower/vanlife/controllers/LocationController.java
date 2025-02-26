1924193dbca771889a9daa1f15f9f15b4c9e1dd5
tbeerbower <tbeerbower@gmail.com>
2025-02-25 20:01:41 -0500

    location controller access control

--- vanlife-backend/src/main/java/org/beerbower/vanlife/controllers/LocationController.java
+++ vanlife-backend/src/main/java/org/beerbower/vanlife/controllers/LocationController.java
@@ -2,12 +2,16 @@ package org.beerbower.vanlife.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.beerbower.vanlife.entities.Location;
+import org.beerbower.vanlife.entities.User;
 import org.beerbower.vanlife.repositories.LocationRepository;
+import org.beerbower.vanlife.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -17,30 +21,40 @@ import java.util.List;
 @RequiredArgsConstructor
 public class LocationController {
     private final LocationRepository locationRepository;
+    private final UserRepository userRepository;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public List<Location> getAllLocations() {
         return locationRepository.findAll();
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}")
     public Location getSingleLocation(@PathVariable long id) {
         return locationRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Location createLocation(@RequestBody Location location) {
+    public Location createLocation(@RequestBody Location location, Principal principal) {
+        User user = userRepository.findByEmail(principal.getName()).orElse(null);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        location.setCreatedBy(user);
+        location.setCreatedAt(LocalDateTime.now());
         return locationRepository.save(location);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteLocation(@PathVariable long id) {
         if (!locationRepository.existsById(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-
         locationRepository.deleteById(id);
     }
 }
