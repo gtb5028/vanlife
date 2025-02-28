5ecccc4893608ac4faf8c52a50b8b811ed5f9830
gtb5028 <41657925+gtb5028@users.noreply.github.com>
2025-02-28 10:11:12 -0800

    Feature: Enable location search by type, name, and proximity (#35)

--- vanlife-backend/src/main/java/org/beerbower/vanlife/repositories/LocationRepository.java
+++ vanlife-backend/src/main/java/org/beerbower/vanlife/repositories/LocationRepository.java
@@ -3,9 +3,17 @@ package org.beerbower.vanlife.repositories;
 import org.beerbower.vanlife.entities.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import java.util.List;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
-    Optional<Location> findByName(String name);
+    // Find locations by name (case-insensitive)
+    List<Location> findByNameContainingIgnoreCase(String name);
+
+    // Find locations by type (case-insensitive)
+    List<Location> findByTypeContainingIgnoreCase(String type);
+
+    // Find locations within a certain area (example: using latitude and longitude)
+    List<Location> findByLatitudeBetweenAndLongitudeBetween(Double minLat, Double maxLat, Double minLon, Double maxLon);
 }
